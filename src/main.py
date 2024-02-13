10f73aa11ec677cd72ce77e0114101bd789fdba8
magnustymoteus <patryk.pilichowski@student.uantwerpen.be>
2024-02-13 14:46:15 +0100

    with tree

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 import antlr4.tree.Trees
 from antlr4 import *
-import sys
 from C.gen.CLexer import *
 from C.gen.CParser import *
+
 
 def tokenizeInput(input_stream):
     lexer = CLexer(input_stream)
     stream = CommonTokenStream(lexer)
     return stream
 
+
 def parseTokens(token_stream):
     parser = CParser(token_stream)
     return parser.expression()
 
-# run with "program.c -s"
+
+# can run with "python src/main.py 'pathtoCprogram.c' -s"
 def main(argv):
     input_stream = FileStream(argv[1])
     tokens = tokenizeInput(input_stream)
     tree = parseTokens(tokens)
+
 
 if __name__ == '__main__':
     main(sys.argv)
