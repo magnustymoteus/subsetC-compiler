a9c68bddb2f067cba58025289e5a156e0563c18a
magnustymoteus <patryk.pilichowski@student.uantwerpen.be>
2024-02-18 21:28:00 +0100

    * generated antlr4 files * main visitor class that inherits from generated visitor * main.py * small change to INT rule in grammar file * added antlr jar file to generate classes from the grammar file

--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,27 @@
-from antlr4 import *
 import sys
+from antlr4.tree.Trees import Trees
+from antlr4 import *
+from src.antlr_files.C_ExpressionsParser import *
+from src.antlr_files.C_ExpressionsLexer import *
 
+def tokenizeInput(input_stream):
+    lexer = C_ExpressionsLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    return stream
+
+
+def parseTokens(token_stream):
+    parser = C_ExpressionsParser(token_stream)
+    return parser
+
+
+# can run with "python src/main.py 'pathtoCprogram.c' -s"
 def main(argv):
-    pass
-
+    input_stream = FileStream("example_source_files/proj1_man_pass_constantFolding.c")
+    tokens = tokenizeInput(input_stream)
+    parser = parseTokens(tokens)
+    tree = parser.expr()
+    print(Trees.toStringTree(tree, None, parser))
 
 if __name__ == '__main__':
     main(sys.argv)
