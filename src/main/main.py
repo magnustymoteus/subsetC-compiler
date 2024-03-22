98c68e356798cc5c3c0f86957bba45315d69a7b8
valeriekat <Valerie.Jacobs@student.uantwerpen.be>
2024-03-22 21:53:13 +0100

    Added flags

--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import argparse
 
 from src.parser.visitor.CST_visitor.cst_to_ast_visitor import CSTToASTVisitor
 from src.parser.visitor.CST_visitor.visualization_visitor import VisualizationVisitor
@@ -13,13 +13,6 @@ from src.parser.visitor.AST_visitor import *
 from src.llvm_target import *
 from src.parser.optimizations import *
 
-"""
-flags to implement: 
-disable-cfold : disable constant folding (should be enabled by default)
-disable-cprog: disable constant propagation (should be enabled by default)
-viz-cst: visualize CST
-viz-ast visualize AST and symbol tables
-"""
 
 # Gives filepath to C_GrammarLexer to generate tokens
 def getTokens(filepath: str):
@@ -50,33 +43,67 @@ def visualizeAST(ast: Ast, filename: str):
     graph = ast.to_dot_graph()
     graph.save(filename=filename)
 
+
 def visualizeCFG(cfg: ControlFlowGraph, filename: str):
     graph = cfg.to_dot_graph()
     graph.save(filename=filename)
 
 
-
 def main(argv):
-    pass_tests = Path("example_source_files").glob('proj2_*_pass_*.c')
-    #syntaxErr_tests = Path("../../example_source_files").glob('proj2_*_syntaxErr_*.c')
+    # Flags
+    """
+    disable-cfold : disable constant folding (should be enabled by default)
+    disable-cprog: disable constant propagation (should be enabled by default)
+    viz-cst: visualize CST
+    viz-ast: visualize AST and symbol tables
+    """
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--disable-cfold', action='store_true',
+                            help='disable constant folding (enabled by default)')
+    arg_parser.add_argument('--disable-cprog', action='store_true',
+                            help='disable constant propagation (enabled by default)')
+    arg_parser.add_argument('--viz-cst', action='store_true', help='visualize CST')
+    arg_parser.add_argument('--viz-ast', action='store_true', help='visualize AST and symbol tables')
+    args = arg_parser.parse_args(argv[1:])
+
+    # pass_tests = Path("example_source_files").glob('proj2_*_pass_*.c')
+    pass_tests = Path("example_source_files").glob('proj2_man_pass_arithmetic.c')
+    # syntaxErr_tests = Path("../../example_source_files").glob('proj2_*_syntaxErr_*.c')
     for path in pass_tests:
         path_in_str = str(path)
+        # Lexes the input file
         tokens = getTokens(path_in_str)
+
         parser = C_GrammarParser(tokens)
-        #parser.addErrorListener(MyErrorListener())
+
+        # parser.addErrorListener(MyErrorListener())
+
+        # tree: list[ProgramContext]
         tree = parser.program()
 
-        #visualizeCST(tree, parser.ruleNames, os.path.basename(path))
+        if args.viz_cst:
+            visualizeCST(tree, parser.ruleNames, os.path.basename(path))
 
+        # conversion from CST to AST
         ast = getAST(tree)
+
         try:
+            # Makes symbol table entries of the ast nodes
             SymbolTableVisitor(ast)
             TypeCheckerVisitor(ast)
+
         except SemanticError as e:
             print(f"{e}")
-        OptimizationVisitor(ast)
-        applyConstantFolding(ast)
-        #visualizeAST(ast, os.path.basename(path) + ".gv")
+
+        if not args.disable_cprog:
+            OptimizationVisitor(ast)
+
+        if not args.disable_cfold:
+            applyConstantFolding(ast)
+
+        if args.viz_ast:
+            visualizeAST(ast, os.path.basename(path) + ".gv")
+
         cfg: ControlFlowGraph = BasicBlockVisitor(ast).cfg
         visualizeCFG(cfg, "viz/cfg-processed/" + str(os.path.basename(path)) + ".gv")
         TACVisitor(cfg)
@@ -89,8 +116,6 @@ def main(argv):
         f = open(f"src/llvm_target/output/{str(os.path.basename(path))}.ll", "w")
         f.write(str(llvm.module))
         f.close()
-
-
 
 
 if __name__ == '__main__':
