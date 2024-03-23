bae1fbffbc4cc3e9a7907ff456899270c2f351ae
magnustymoteus <patryk.pilichowski@student.uantwerpen.be>
2024-03-23 17:58:02 +0100

    - proj3_man_semanticErr_typedef.c -> proj3_man_syntaxErr_typedef2.c

--- example_source_files/proj3_man_semanticErr_typedef.c
+++
@@ -1,8 +0,0 @@
-
-
-// type already exists (patryk: this should be a syntax error instead)
-typedef float int;
-
-int main() {
-    int x = 4;
-}
---
+++ example_source_files/proj3_man_syntaxErr_typedef2.c
@@ -0,0 +1,8 @@
+
+
+// type already exists (patryk: shouldn't this be a syntax error? a primitive datatype as alias should never be even allowed)
+typedef float int;
+
+int main() {
+    int x = 4;
+}
