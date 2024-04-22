562d4057336a5f4c131223800aedc9793cebd384
Patryk Pilichowski <patryk.pilichowski@student.uantwerpen.be>
2024-04-22 14:19:22 +0200

    simplifier visitor, more array checks and llvm loads

--- included2.c
+++
@@ -1,7 +0,0 @@
-#ifndef INCLUDED_H
-#define INCLUDED_H
-#include "included3.c"
-void print_test(float a) {
-    printf("%f", a);
-}
-#endif
--- included3.c
+++
@@ -1,7 +0,0 @@
-#ifndef INCLUDED2_H
-#define INCLUDED2_H
-int print_hey() {
-    printf("%c", 'h');
-    return 0;
-}
-#endif
