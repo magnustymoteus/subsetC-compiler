d1e74d4ca3ce67a3e62f7fa339b399aeb29d93ca
Patryk Pilichowski <patryk.pilichowski@student.uantwerpen.be>
2024-05-09 21:08:16 +0200

    && and || llvm bugfixes

--- src/compilation/codegen/LLVM_codegen/mapping/float/binary_op.py
+++ src/compilation/codegen/LLVM_codegen/mapping/float/binary_op.py
@@ -6,6 +6,9 @@ def get_float_boolean_binary_op(left_value: ir.Instruction | ir.Constant, right_
                           builder: ir.IRBuilder, create_reg: Callable, boolean_op: Callable) -> ir.Instruction:
     return builder.zext(boolean_op(), ir.IntType(32), create_reg())
 
+def get_float_logical_comp_op(op: Callable, builder: ir.IRBuilder, create_reg: Callable):
+    return builder.zext(op(), ir.IntType(32), create_reg())
+
 def get_float_binary_op(left_value: ir.Instruction | ir.Constant, right_value: ir.Instruction | ir.Constant, operator: str,
                         builder: ir.IRBuilder, create_reg: Callable) -> Callable:
     match operator:
@@ -26,13 +29,19 @@ def get_float_binary_op(left_value: ir.Instruction | ir.Constant, right_value: i
         case "|":
             return lambda: builder.or_(left_value, right_value, create_reg())
         case "||":
-            return lambda: builder.or_(left_value, right_value, create_reg())
+            rel_op = lambda: builder.or_(
+                builder.fcmp_unordered('!=', left_value, ir.Constant(ir.FloatType(), 0.0)),
+                builder.fcmp_unordered('!=', right_value, ir.Constant(ir.FloatType(), 0.0)), create_reg())
+            return lambda: get_float_relational_comp_op(rel_op, builder, create_reg)
         case "^":
             return lambda: builder.xor(left_value, right_value, create_reg())
         case "&":
             return lambda: builder.and_(left_value, right_value, create_reg())
         case "&&":
-            return lambda: builder.and_(left_value, right_value, create_reg())
+            rel_op = lambda: builder.and_(
+                builder.fcmp_unordered('!=', left_value, ir.Constant(ir.FloatType(), 0.0)),
+                builder.fcmp_unordered('!=', right_value, ir.Constant(ir.FloatType(), 0.0)), create_reg())
+            return lambda: get_float_relational_comp_op(rel_op, builder, create_reg)
         case _:
             comp_op: Callable = lambda: builder.fcmp_unordered(operator, left_value, right_value, create_reg())
             return lambda: get_float_boolean_binary_op(left_value, right_value, operator, builder, create_reg, comp_op)
--- src/compilation/codegen/LLVM_codegen/mapping/int/binary_op.py
+++ src/compilation/codegen/LLVM_codegen/mapping/int/binary_op.py
@@ -5,7 +5,8 @@ def get_int_boolean_binary_op(left_value: ir.Instruction | ir.Constant, right_va
                           operator: str,
                           builder: ir.IRBuilder, create_reg: Callable, boolean_op: Callable) -> ir.Instruction:
     return builder.zext(boolean_op(), ir.IntType(32), create_reg())
-
+def get_int_logical_comp_op(op: Callable, builder: ir.IRBuilder, create_reg: Callable):
+    return builder.zext(builder.icmp_signed('!=', op(), ir.Constant(ir.IntType(32), 0)), ir.IntType(32), create_reg())
 def get_int_binary_op(left_value: ir.Instruction | ir.Constant, right_value: ir.Instruction | ir.Constant, operator: str,
                              builder: ir.IRBuilder, create_reg: Callable) -> Callable:
     match operator:
@@ -26,13 +27,15 @@ def get_int_binary_op(left_value: ir.Instruction | ir.Constant, right_value: ir.
         case "|":
             return lambda: builder.or_(left_value, right_value, create_reg())
         case "||":
-            return lambda: builder.or_(left_value, right_value, create_reg())
+            rel_op= lambda: builder.or_(left_value, right_value, create_reg())
+            return lambda: get_int_relational_comp_op(rel_op, builder, create_reg)
         case "^":
             return lambda: builder.xor(left_value, right_value, create_reg())
         case "&":
             return lambda: builder.and_(left_value, right_value, create_reg())
         case "&&":
-            return lambda: builder.and_(left_value, right_value, create_reg())
+            rel_op = lambda: builder.and_(left_value, right_value, create_reg())
+            return lambda: get_int_relational_comp_op(rel_op, builder, create_reg)
         case _:
             comp_op: Callable = lambda: builder.icmp_signed(operator, left_value, right_value, create_reg())
             return lambda: get_int_boolean_binary_op(left_value, right_value, operator, builder, create_reg, comp_op)
