31b241be50e26a7c945b0acf9f8a8baa60b5688c
valeriekat <Valerie.Jacobs@student.uantwerpen.be>
2024-06-05 01:08:41 +0200

    fixed fundamental test

--- src/tests/mips_output_test.py
+++ src/tests/mips_output_test.py
@@ -4,8 +4,13 @@ from src.compilation.compiler import *
 import subprocess
 import re
 
+
 all = Path("../../example_source_files/CorrectCode").glob("*.c")
-fundamental = Path("../../example_source_files/CorrectCode/").glob("*pass*.c")
+
+fundamental = Path("../../example_source_files/CorrectCode/")
+regex = re.compile(r"proj[123].*pass.*\.c$")
+
+matching_files = [f for f in fundamental.glob("*.c") if regex.match(f.name)]
 
 
 # gives all files that match the regex
@@ -41,11 +46,10 @@ def test_mips_fundamentals():
     failed = False
     compiler = Compiler({"cprop", "cfold", "dcode"}, None)
 
-
-    filtered = ['proj2_man_pass_nestedCompoundStmts.c', 'proj4_man_pass_switch.c']
+    filtered = []
 
     tested_files: int = 0
-    filtered_all = [file for file in fundamental if file.name not in filtered]
+    filtered_all = [file for file in matching_files if file.name not in filtered]
 
     for path in filtered_all:
         tested_files += 1
@@ -58,14 +62,14 @@ def test_mips_fundamentals():
         tested_output = subprocess.check_output(["mars", f"./{filename}/mips.asm", "nc"])
         # remove trailing newline
         tested_output = tested_output[:-1]
-        subprocess.run(["gcc", "-std=gnu99", "-w", path])
+        subprocess.run(["gcc", "-ansi", "-pedantic", path])
         reference_output = subprocess.check_output([f"./a.out"])
         success = tested_output == reference_output
         success_str = "✔" if success else "X"
         print(f"{success_str}{path}")
         print(f"expected: {reference_output}")
         print(f"actual: {tested_output}")
-        # print("tested: " + str(tested_files))
+        print("tested: " + str(tested_files))
         print()
         if not success:
             failed = True
@@ -75,28 +79,32 @@ def test_mips_fundamentals():
 
 def test_all_mips():
     failed = False
-    compiler = Compiler(None, {"all"})
-    # filtered = ["prime.c", "fibonacciRecursive.c"]
-    # filtered = ["proj2_floatcmp.c", "proj2_man_pass_"]
-    filtered = ["comparisons1.c"]
+    compiler = Compiler({"cprop", "cfold", "dcode"}, None)
+
+    filtered = []
+
+    tested_files: int = 0
     filtered_all = [file for file in all if file.name not in filtered]
+
     for path in filtered_all:
+        tested_files += 1
         filename = str(os.path.splitext(os.path.basename(path))[0])
         print("\nFilename:" + filename + ".c")
         llvm = compiler.compile_llvm(path)
         mips = compiler.compile_mips(llvm)
         compiler.export_mips(mips, Path(f"./{filename}/mips.asm"))
-        # nc option to remove
+        # nc option to remove standard output text
         tested_output = subprocess.check_output(["mars", f"./{filename}/mips.asm", "nc"])
         # remove trailing newline
         tested_output = tested_output[:-1]
-        subprocess.run(["gcc", "-std=gnu99", "-w", path])
+        subprocess.run(["gcc", "-ansi", "-pedantic", path])
         reference_output = subprocess.check_output([f"./a.out"])
         success = tested_output == reference_output
         success_str = "✔" if success else "X"
         print(f"{success_str}{path}")
         print(f"expected: {reference_output}")
         print(f"actual: {tested_output}")
+        print("tested: " + str(tested_files))
         print()
         if not success:
             failed = True
