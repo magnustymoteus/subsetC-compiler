c59171b2fecac6829928615d92f3f86d00980953
valeriekat <Valerie.Jacobs@student.uantwerpen.be>
2024-06-04 21:57:39 +0200

    adjusted tests

--- src/tests/mips_output_test.py
+++ src/tests/mips_output_test.py
@@ -40,37 +40,9 @@ def get_files_containing_char(filepath, regex):
 def test_mips_fundamentals():
     failed = False
     compiler = Compiler({"cprop", "cfold", "dcode"}, None)
-    """
-    °has printf in tests:
-        - proj3_man_pass_printf.c
-        - proj4_man_pass_whileLoop.c
-        - proj4_man_pass_assignmentExample.c
-        - proj4_man_pass_forLoop.c
-        - proj4_man_pass_if.c
-        - proj4_man_pass_ifChar.c
-        - proj4_man_pass_ifEnum.c
-        - proj4_man_pass_ifFloat.c
-        - proj4_man_pass_switch.c
-        - proj4_man_pass_switchChar.c
-        - proj4_man_pass_switchEnum.c
-        - proj4_man_pass_whileLoop.c
-    """
 
-    filtered = [
-        # b'10Error in proj4_man_pass_whileLoop/mips.asm line 148: Runtime exception at 0x0040018c: address out of range 0x00000000\n\nProcessing terminated due to errors.'
-        "proj4_man_pass_whileLoop.c",
-        # proj4_man_pass_forLoop/mips.asm line 164: Runtime exception at 0x004001d0: arithmetic overflow\n\nProcessing terminated due to errors.\n'
-        "proj4_man_pass_forLoop.c",
-        # line 326: Runtime exception at 0x00400390: address out of range 0x00000000\n\nProcessing terminated due to errors.\n'
-        "proj4_man_pass_assignmentExample.c",
-        # Runtime exception at 0x00400238: address out of range 0x00000000
-        "proj4_man_pass_switch.c",
-        # doesn't work when cprop is enabled
-        "proj4_man_pass_switchEnum.c",
-        "proj4_man_pass_switchChar.c",
-        # printf of float understandably wrong
-        "proj4_man_pass_ifChar.c",
-    ]
+
+    filtered = ['proj2_man_pass_nestedCompoundStmts.c', 'proj4_man_pass_switch.c']
 
     tested_files: int = 0
     filtered_all = [file for file in fundamental if file.name not in filtered]
